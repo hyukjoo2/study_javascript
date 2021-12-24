06c76b71bb7f2d24ab5723e862618375bd9ba9e4
Lee, Hyuk Joo <hyuk.joo.lee@sap.com>
2021-12-24 19:57:55 +0900

    array

---
+++ js/array.js
@@ -0,0 +1,86 @@
+'use strict'
+
+// Array
+
+// 1. Declaration
+const arr1 = new Array();
+const arr2 = [1, 2];
+
+// 2. Index position
+const fruits = ['apple', 'banana'];
+console.log(fruits);
+console.log(fruits.length);
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(fruits[fruits.length - 1]);
+
+// 3. Looping over an array
+// print all fruits
+// a. for
+for (let i =0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+
+// b. for of
+for (let fruit of fruits) {
+    console.log(fruit);
+}
+
+// c. forEach
+fruits.forEach((fruit) => console.log(fruit));
+
+// 4. Addition, deletion, copy
+// push: add an item to the end
+fruits.push('berry', 'peach');
+console.log(fruits);
+
+// pop: remove an item from the end
+fruits.pop();
+console.log(fruits);
+fruits.pop();
+console.log(fruits);
+
+// unshift: add an item to the beginning
+fruits.unshift('berry', 'lemon');
+console.log(fruits);
+
+// shift: remove an item from the beginning
+fruits.shift();
+console.log(fruits);
+fruits.shift();
+console.log(fruits);
+
+// note!! shift, unshift are slower than pop, push
+
+// splice: remove an item by index position
+fruits.push('berry', 'peach', 'lemon')
+console.log(fruits);
+// fruits.splice(1); // remove all from 1
+// fruits.splice(1, 1);
+fruits.splice(1, 1, 'green apple', 'water melon');
+console.log(fruits);
+
+// combine two arrays
+const fruits2 = ['kiwi', 'coconut'];
+const newFruits = fruits.concat(fruits2);
+console.log(newFruits);
+
+// 5. Searching
+// indexOf: find the index
+console.clear();
+console.log(fruits);
+console.log(fruits.indexOf('apple'));
+console.log(fruits.indexOf('water melon'));
+console.log(fruits.indexOf('coconut'));
+
+// includes
+console.log(fruits.includes('water melon'));
+console.log(fruits.includes('coconut'));
+
+// lastIndexOf
+console.clear();
+fruits.push('apple');
+console.log(fruits);
+console.log(fruits.indexOf('apple'));
+console.log(fruits.lastIndexOf('apple'));
